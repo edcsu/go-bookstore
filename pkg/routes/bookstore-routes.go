92e504dc0dd27fec7437f1189203f9705a7eef77
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-05-21 21:17:12 +0300

    feat: add route function

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("api/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("api/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("api/book/{bookId}", controllers.GetBook).Methods("GET")
+	router.HandleFunc("api/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("api/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
